8905ae8d5d1e227328a4d3dc05b45b38527a64e2
Robert Heinbokel <robertheinbokel@gmail.com>
2024-01-17 10:48:08 -0500

    Added Person class and created new person in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CIS_106_WEEK_3_GIT_INTRO;
+
+Person person = new Person{FirstName = "Bob", LastName = "Heinbokel"};
+
+Console.WriteLine($"This guy is named {person.FirstName} {person.LastName}");
---
+++ models/Person.cs
@@ -0,0 +1,7 @@
+namespace CIS_106_WEEK_3_GIT_INTRO {
+
+    class Person {
+        public string? FirstName {get; set;}
+        public string? LastName {get; set;}
+    }
+}
